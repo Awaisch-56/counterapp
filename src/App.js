814739f9cc3f7de9c2c11c90b255a7a97bbffb67
Awaisch-56 <awaisakhtar395@gmail.com>
2023-09-06 11:46:45 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Counter App</h1>
+        <p>Current Count: {count}</p>
+        <div>
+          <button onClick={increment}>Increment</button>
+          <button onClick={reset}>Reset</button>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
